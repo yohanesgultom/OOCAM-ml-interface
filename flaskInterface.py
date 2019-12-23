063f4b6e69ac0ecaa8acd5078c2865755d8c7b1c
Eehit Ray <greatblitz982@gmail.com>
2019-12-24 00:52:41 +0530

    updated filename on static serve

--- flaskInterface.py
+++ flaskInterface.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("interface.html")
+    return render_template("HTML_Interface.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
