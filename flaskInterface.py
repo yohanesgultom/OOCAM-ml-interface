3bc22e3c71cf28b3eb7b486881faadd466d4a9c5
Eehit Ray <greatblitz982@gmail.com>
2019-12-24 01:44:50 +0530

    model interface wip

--- flaskInterface.py
+++ flaskInterface.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os, modelTrainScript
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("HTML_Interface.html")
+
+@app.route("/train", methods = ["POST", "GET"])
+def train():
+    if request.method == "POST":
+        folder_path = request.form['folder_path']
+        
 
 if __name__ == '__main__':
     app.run(debug = True)
