e92ef45646df1b033f71edcae6a89703733373b7
Eehit Ray <greatblitz982@gmail.com>
2020-01-12 05:20:10 +0800

    Created download utilities

---
+++ downloadUtils.py
@@ -0,0 +1,11 @@
+from zipfile import ZipFile
+from flask import send_file
+import os
+
+def zipAndDownload(folder):
+    with ZipFile(f'{folder}.zip', 'w') as zipObj:
+        for folderName, subfolders, filenames in os.walk(folder):
+            for filename in filenames:
+                zipObj.write(os.path.join(folderName, filename))
+
+    send_file(f'{folder}.zip', as_attachment = True)
