7ee4ee77c3996d4decdaf0f184e28e2d19a60305
Yohanes Gultom <yohanes.gultom@gmail.com>
2020-04-21 12:53:35 +0700

    fix installer.bat to make it work on windows 10; modify run.bat to display message when no virtual environment found

--- installer.bat
+++ installer.bat
@@ -8,50 +8,49 @@ set PYTHON_SCRIPTS=%PYTHON_HOME%\Scripts
 set PYTHON_PIP_EXE=%PYTHON_SCRIPTS%\pip3.7.exe
 
 if not exist %PYTHON_EXE% (
-echo,
-echo ------------------------------------------------------------------
-echo Downloading Python...
-echo ------------------------------------------------------------------
-echo,
 
-if not exist %PYTHON_MSI% (
-    curl -L -O http://python.org/ftp/python/%PYTHON_VERSION%/%PYTHON_MSI%
-)
-)
+	if not exist %PYTHON_MSI% (
+		echo,
+		echo ------------------------------------------------------------------
+		echo Downloading Python...
+		echo ------------------------------------------------------------------
+		echo,
 
-if not exist %PYTHON_EXE% (
-echo,
-echo ------------------------------------------------------------------
-echo Installing Python..
-echo ------------------------------------------------------------------
-echo,
+		curl -L -O http://python.org/ftp/python/%PYTHON_VERSION%/%PYTHON_MSI%
+	)
 
-if exist %PYTHON_MSI% (
-    msiexec.exe /qb /i %PYTHON_MSI% ALLUSERS=1 ADDLOCAL=ALL
-) else (
-    echo Python installer package didn't seem to download correctly.
-    exit /b 1
-)
+	echo,
+	echo ------------------------------------------------------------------
+	echo Installing Python..
+	echo ------------------------------------------------------------------
+	echo,
+
+	if exist %PYTHON_MSI% (
+        %PYTHON_MSI% /quiet InstallAllUsers=1 TargetDir=%PYTHON_HOME% Include_pip=1
+	) else (
+		echo Python installer package didn't seem to download correctly.
+		exit /b 1
+	)
 )
 
 if not exist %PYTHON_SCRIPTS%\virtualenv.exe (
-echo,
-echo ------------------------------------------------------------------
-echo Installing virtualenv
-echo ------------------------------------------------------------------
-echo,
+	echo,
+	echo ------------------------------------------------------------------
+	echo Installing virtualenv
+	echo ------------------------------------------------------------------
+	echo,
 
-%PYTHON_PIP_EXE% install virtualenv
+	%PYTHON_PIP_EXE% install virtualenv
 )
 
 if not exist venv\Scripts\activate (
-echo,
-echo ------------------------------------------------------------------
-echo Creating virtual environment
-echo ------------------------------------------------------------------
-echo,
+	echo,
+	echo ------------------------------------------------------------------
+	echo Creating virtual environment
+	echo ------------------------------------------------------------------
+	echo,
 
-%PYTHON_SCRIPTS%\virtualenv.exe venv -p %PYTHON_EXE%
+	%PYTHON_SCRIPTS%\virtualenv.exe venv -p %PYTHON_EXE%
 )
 
 echo,
--- run.bat
+++ run.bat
@@ -1,1 +1,13 @@
-venv\Scripts\python.exe flaskInterface.py
+@echo off
+
+if exist venv\Scripts\python.exe (
+	echo,
+	echo ------------------------------------------------------------------
+	echo Running with virtual environment
+	echo ------------------------------------------------------------------
+	echo,
+
+	venv\Scripts\python.exe flaskInterface.py
+) else (
+    echo No virtual environment found. Please run installer.bat first
+)
